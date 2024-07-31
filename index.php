696d18b55f894b84cdd1363e1b5a6b22a385e5ef
KamoMabye <Kamomabye7@gmail.com>
2024-07-31 13:46:14 +0200

    changed index.html to index.php for testing

---
+++ index.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="EasyNet: In Business Communications provides IT solutions with various IT services and products." />
+  <title>EasyNet IBC | IT Solutions</title>
+  <link rel="shortcut icon" type="image/png" href="_images/_logos/easynet_icon.png">
+  <link href="_styles/style.css" rel="stylesheet" />
+  <link href="_styles/font-awesome.css" rel="stylesheet" />
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+  <header>
+    <div id="left">
+      <a href="index.html"><img src="_images/_logos/easynet.png" id="logo" width="120px" title="EasyNet Homepage" /></a>
+    </div>
+    <input type="checkbox" id="check">
+    <div id="middle">
+      <nav>
+        <ul id="nav_content">
+          <li id="nav_link">
+            <a href="index.html" id="nav_text" class="active">Home</a>
+          </li>
+          <li id="nav_link">
+            <a href="about.html" id="nav_text">About Us</a>
+          </li>
+          <li id="nav_link">
+            <a href="products.html" id="nav_text">Products</a>
+            <div class="dropdown">
+              <div class="dropdown-content">
+                <div class="row">
+                  <h4><a href="products2.html">Hardware</a></h4>
+                  <ul class="mega-link">
+                    <li>Servers</li>
+                    <li>Desktops</li>
+                    <li>Monitors</li>
+                    <li>Fax Machines</li>
+                    <li>Computer Components</li>
+                    <li>Projectors</li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <h4><a href="products2.html">Software</a></h4>
+                  <ul class="mega-link">
+                    <li>Microsoft</li>
+                    <li>Symantec</li>
+                    <li>CorelDraw</li>
+                    <li>Adobe</li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <h4><a href="products2.html">Accessories</a></h4>
+                  <ul class="mega-link">
+                    <li>Printer Cartridge</li>
+                    <li>Headsets</li>
+                    <li>Controllers</li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <h4><a href="products2.html">Combos</a></h4>
+                  <ul class="mega-link">
+                    <li>Gaming</li>
+                    <li>Keyboard and Mouse</li>
+                    <li>Sound System</li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <h4><a href="products2.html">All</a></h4>
+                  <ul class="mega-link">
+                    <li>Asus</li>
+                    <li>Acer</li>
+                    <li>Apple</li>
+                    <li>Dell</li>
+                    <li>Hisense</li>
+                    <li>Hp</li>
+                    <li>Lenovo</li>
+                    <li>Microsoft</li>
+                    <li>Samsung</li>
+                  </ul>
+                </div>
+              </div>
+          </li>
+          <li id="nav_link">
+            <a href="client.html" id="nav_text">Partners and Clients</a>
+          </li>
+          <li id="nav_link">
+            <a href="contact.html" id="nav_text">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <div id="right">
+      <p>
+        <a href="register.html" id="loginlinks">Sign Up</a> /
+        <a href="login.html" id="loginlinks">Log In</a>
+      </p>
+      <div id="right-item">
+        <a href="favourites.html"><img id="icons_heart" src="_images/_icons/heart.png" width="30px" /></a>
+        <a href="checkout.html"><img id="icons_bag" src="_images/_icons/bag.png" width="30px" /></a>
+      </div>
+
+      <label for="check" class="menu">
+        <i class='bx bx-menu' id="menu_icon"></i>
+        <i class='bx bx-x' id="close_icon"></i>
+      </label>
+
+    </div>
+
+    </div>
+    <script src="_javascript/index.js"></script>
+  </header>
+
+  <body>
+    <div id="background_home">
+      <h1>EasyNet</h1>
+      <h2>In Business Communications</h2>
+      <h3>Providing IT solutions in an easy way</h2><br>
+        <h2>Shop our wide range of gadgets and equipment</h2><br>
+        <a href="products.html"><button>Shop now </button> </a>
+    </div>
+
+    <div id="feature_box">
+      <h1 id="feature_box_head">Deals</h1>
+      <p>Explore our massive deals on all products</p>
+    </div>
+
+    <section id="features">
+      <div id="fea-container">
+        <span>SAVE 300</span>
+        <div id="fea">
+          <img src="_images/adobe.jpg" width="200px">
+        </div>
+        <h6>Adobe Acrobat</h6>
+        <h4>Abobe creative suite 4</h4>
+        <h7>800</h7>
+        <h5>R500</h5>
+        <div id="star">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/half-filled-rating-star.png" width="15px">
+        </div>
+        <a href="products2.html">
+          <button id="boxbutton-home">Add to Cart</button></a>
+      </div>
+
+      <div id="fea-container">
+        <span>SAVE 2000</span>
+        <div id="fea">
+          <img src="_images/macbook_15.webp" width="200px">
+        </div>
+        <h6>Apple</h6>
+        <h4>Macbook 15inch</h4>
+        <h7>27000</h7>
+        <h5>R25000</h5>
+        <div id="star">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/half-filled-rating-star.png" width="15px">
+        </div>
+        <a href="products2.html">
+          <button id="boxbutton-home">Add to Cart</button></a>
+      </div>
+
+      <div id="fea-container">
+        <span>SAVE 1500</span>
+        <div id="fea">
+          <img src="_images/xbox_seriesS.webp" width="200px">
+        </div>
+        <h6>Xbox</h6>
+        <h4>Xbox Series S</h4>
+        <h7>9000</h7>
+        <h5>R7500</h5>
+        <div id="star">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/half-filled-rating-star.png" width="15px">
+        </div>
+        <a href="products2.html">
+          <button id="boxbutton-home">Add to Cart</button></a>
+      </div>
+
+      <div id="fea-container">
+        <span>SAVE 1500</span>
+        <div id="fea">
+          <img src="_images/ps5.jpg" width="150px">
+        </div>
+        <h6>Playstation</h6>
+        <h4>Playstation 5 Disc</h4>
+        <h7>1500</h7>
+        <h5>R13500</h5>
+        <div id="star">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/half-filled-rating-star.png" width="15px">
+        </div>
+        <a href="products2.html">
+          <button id="boxbutton-home">Add to Cart</button></a>
+      </div>
+
+    </section>
+
+    <section id="deals_banner">
+      <h4> Combos</h4>
+      <h2>Explore our - <span> EXCLUSIVE COMBO </span> - deals</h2>
+      <button>Explore More</button>
+    </section>
+
+    <div id="feature_box">
+      <h1 id="feature_box_head">Featured Items</h1>
+      <p>View our most purchased items</p>
+    </div>
+    <section id="features">
+      <div id="fea-container">
+        <div id="fea">
+          <img src="_images/microsoft_365.webp" width="200px">
+        </div>
+        <h6>Microsoft</h6>
+        <h4>Microsoft 365</h4>
+        <h5>R1 200</h5>
+        <div id="star">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/half-filled-rating-star.png" width="15px">
+          <img src="_images/_icons/empty-rating.png" width="15px">
+        </div>
+        <a href="products2.html">
+          <button id="boxbutton-home">Add to Cart</button></a>
+      </div>
+
+      <div id="fea-container">
+        <div id="fea">
+          <img src="_images/macbook_13.webp" width="200px">
+        </div>
+        <h6>Apple</h6>
+        <h4>Apple MacBook Air 13-Inch With M1 Processor</h4>
+        <h5>R15 999</h5>
+        <div id="star">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/half-filled-rating-star.png" width="15px">
+          <img src="_images/_icons/empty-rating.png" width="15px">
+        </div>
+        <a href="products2.html">
+          <button id="boxbutton-home">Add to Cart</button></a>
+      </div>
+
+      <div id="fea-container">
+        <div id="fea">
+          <img src="_images/ipad_13inch.webp" width="200px">
+        </div>
+        <h6>Apple</h6>
+        <h4>Apple iPad Pro 13inch M4 WiFi 256GB Standard Glass Space Black</h4>
+        <h5>R15 999</h5>
+        <div id="star">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+        </div>
+        <a href="products2.html">
+          <button id="boxbutton-home">Add to Cart</button></a>
+      </div>
+
+      <div id="fea-container">
+        <div id="fea">
+          <img src="_images/playstation_controller.webp" width="250px">
+        </div>
+        <h6>PlayStation</h6>
+        <h4>Playstation 5 DualSense EDGE Wireless Controller</h4>
+        <h5>R4 599</h5>
+        <div id="star">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/full-filled-rating.png" width="15px">
+          <img src="_images/_icons/half-filled-rating-star.png" width="15px">
+        </div>
+        <a href="products2.html">
+          <button id="boxbutton-home">Add to Cart</button></a>
+      </div>
+    </section>
+
+    <div id="home_box">
+      <h1 id="home_box_head">Shop our products</h1>
+    </div>
+    <div id="homeboxes">
+      <div id="prodbox5">
+        <img src="_images/hardware.png" width="250px" />
+        <h3>Hardware</h3>
+        <a href="products2.html"><button id="boxbutton">Shop Hardware</button></a>
+      </div>
+      <div id="prodbox5">
+        <img src="_images/adobe.jpg" width="250px" />
+        <h3>Software</h3>
+        <a href="products2.html"><button id="boxbutton">Shop Software</button></a>
+      </div>
+      <div id="prodbox5">
+        <img src="_images/consumables.webp" width="250px" />
+        <h3>Accessories</h3>
+        <a href="products2.html"><button id="boxbutton">Shop Accessories</button></a>
+      </div>
+      <div id="prodbox5">
+        <img src="_images/all_products.jpg" width="265px" />
+        <h3>All Products</h3>
+        <a href="products2.html"><button id="boxbutton">Shop All Products</button></a>
+      </div>
+    </div>
+
+
+    <section id="bottom_banners">
+      <div class="info_banner1">
+        <h2>Find Out More About Us</h2>
+        <p>Find out more about what we do and how we can assist you.
+          </br>
+          <a href="about.html" id="paragraph_link">About Us ></a>
+        </p>
+      </div>
+
+      <div class="info_banner2">
+        <h2>Products and Services</h2>
+        <p>Check out our various IT products and services.</p></br>
+        <a href="products.html" id="paragraph_link">Products and Services ></a>
+      </div>
+
+      <div class="info_banner3">
+        <h2>Our Partners and Clients</h2>
+        <p>Check out our Partners and Clients that we currently work with.</br>
+          <a href="client.html" id="paragraph_link">Partners and Clients ></a>
+        </p>
+      </div>
+
+      <div class="info_banner4">
+        <h2>Talk to Us</h2>
+        <p>Find out how you can contact us.
+          </br>
+          <a href="contact.html" id="paragraph_link">Contact Us ></a>
+        </p>
+      </div>
+    </section>
+    <script src="_javascript/index.js"></script>
+  </body>
+
+  <footer>
+    <div class="social-media-icons">
+      <a href="https://www.facccebook.com/" target="_blank"> <i class='bx bxl-facebook-circle'>
+          <p>Facebook</p>
+        </i>
+      </a>
+      <a href="https://www.innnstagram.com/" target="_blank"> <i class='bx bxl-instagram'>
+          <p>Instagram</p>
+        </i>
+      </a>
+      <a href="https://www.linkkkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit" target="_blank"><i class='bx bxl-linkedin-square'>
+          <p>LinkedIn</p>
+        </i>
+      </a>
+    </div>
+    <hr id="foot_line" />
+    <p id="footer_text">
+      Copyright &copy; 2024 EasyNet In Business Communications
+    </p>
+  </footer>
+</body>
+
+</html>
