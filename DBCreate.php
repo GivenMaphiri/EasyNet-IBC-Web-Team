4e0432d6a4134f89c1c65ea77b44ea3e90073c9b
GivenMaphiri <65910013+GivenMaphiri@users.noreply.github.com>
2024-09-02 13:23:24 +0200

    changed orders table (delete & read)

--- DBCreate.php
+++ DBCreate.php
@@ -84,12 +84,13 @@ $sql = "CREATE TABLE IF NOT EXISTS  orders (
     prod_ID INT(11) NOT NULL,
     order_total DECIMAL(10, 2) NOT NULL,
     placed_on DATETIME NOT NULL DEFAULT CURRENT_TIME,
-    shipping_address VARCHAR(255), 
+    shipping_ID INT(11), 
     payment_method VARCHAR(255) NOT NULL, 
     payment_status ENUM('Paid', 'Due', 'Refunded') NOT NULL,
     status ENUM('Pending', 'In Progress', 'Delivered') NOT NULL,
     FOREIGN KEY (user_ID) REFERENCES users(user_ID), 
     FOREIGN KEY (prod_ID) REFERENCES products(prod_ID)
+    FOREIGN KEY (shipping_ID) REFERENCES shipping(shipping_ID)
 )";
 
 if ($conn->query($sql) === TRUE) {
