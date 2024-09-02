8a89c699369aaac02a07721ddaf7f58cbd428755
Mckhyle Ganas23 <104443613+MckhyleGanas@users.noreply.github.com>
2024-09-02 14:00:53 +0200

    Linked message icon (admin-orders)

--- orders.php
+++ orders.php
@@ -106,14 +106,13 @@ $result = $conn->query($sql);
     <header>
       <div class="menu-toggle">
         <label for="sidebar-toggle">
-          <span class="las la-bars"></span>
+         <!-- <span class="las la-bars"></span> -->
         </label>
       </div>
 
       <div class="header-icons">
-        <span class="las la-search"></span>
-        <span class="las la-bookmark"></span>
-        <span class="las la-sms"></span>
+        
+        <a href="messages.php"><span class="las la-sms"></span></a>
       </div>
     </header>
 
